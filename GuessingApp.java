0893800347bdb86027727add1df7c4f6dcb8b231
RITHVIK-KASHYAP <rithvik052003@gmail.com>
2026-02-23 14:43:15 +0530

    Use Case 3

--- GuessingApp.java
+++ GuessingApp.java
@@ -1,31 +1,24 @@
-import java.util.Random;
-public class GuessingApp {
-    public static void main(String[] args){
-        System.out.println("Welcome to Guessing App");
-        GameConfig gameConfig=new GameConfig();
-        gameConfig.showRules();
-    }
+import java.util.Scanner;
 
-    
-}
-class GameConfig
-{
-    private final int MIN = 1;
-    private final int MAX = 100;
-    private final int MAX_ATTEMPTS= 7;
-    private final int MAX_HINTS = 3;
-    int targetNumber;
-    public GameConfig(){
-        Random random = new Random();
-        this.targetNumber=random.nextInt(MAX-MIN +1)+MIN;
-    }
-    public int getTargetNumber(){ return targetNumber;}
-    public int getMaxAttempts(){return MAX_ATTEMPTS;}
-    public int getMaxHints(){ return MAX_HINTS; }
-    public void showRules(){
-        System.out.println("Guess a number between"+MIN +" and "+MAX);
-        System.out.println("You Are "+MAX_ATTEMPTS);
-        System.out.println("Hints will be provided after wrong guesses.\n");
+public class GuessingApp {
+    public static void main(String[] args)  {
+        System.out.println("Welcome to Guessing App");
+        GameConfig config=new GameConfig();
+        config.showRules();
+        Scanner scanner = new Scanner(System.in);
+        int attempts =0;
+        while(attempts<config.getMaxAttempts())
+        {
+            System.out.print("Enter your guess:");
+            int guess = scanner.nextInt();
+            attempts++;
+            String result= GuessValidator.validateGuess(guess, config.getTargetNumber());
+            System.out.println(result);
         
+        if ("CORRECT".equals(result))
+        {
+            break;
+        }
     }
+}    
 }
