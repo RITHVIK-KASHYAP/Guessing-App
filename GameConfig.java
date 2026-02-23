f73d1af9c23d298b7934c1ce684527559045bb35
RITHVIK-KASHYAP <rithvik052003@gmail.com>
2026-02-23 17:17:00 +0530

    [Rithvik] Coded hints for game

--- GameConfig.java
+++ GameConfig.java
@@ -1,3 +1,10 @@
+/*
+Guessing App UC1 - Coded to show rules of the game 
+
+@author Rithvik
+@version 3.0
+
+*/
 import java.util.Random;
 
 public class GameConfig
--- GuessValidator.java
+++ GuessValidator.java
@@ -1,3 +1,10 @@
+/*
+Guessing App UC1 - Coded to show rules of the game 
+
+@author Rithvik
+@version 3.0
+
+*/
 public class GuessValidator
 {
     public static String validateGuess(int guess, int target)
