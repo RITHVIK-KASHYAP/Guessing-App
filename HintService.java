cc3aed0497772a7a0eabd676f39c1046b180bd70
RITHVIK-KASHYAP <rithvik052003@gmail.com>
2026-02-24 10:04:02 +0530

    [Rithvik] Coded to handle Exceptions in game

--- HintService.java
+++ HintService.java
@@ -1,10 +1,3 @@
-/*
-Guessing App UC1 - Coded to show rules of the game 
-
-@author Rithvik
-@version 4.0
-
-*/
 public  class HintService
 {
     public static String generateHint(int target, int hintCount)
